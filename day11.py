e9f7e4d96fef696f3d719235c511924293a4d086
LeeSH <tkdgurdhkd99@naver.com>
2025-02-24 14:46:00 +0900

    v0.3 day11 LinearRegression

--- day11.py
+++ day11.py
@@ -1,10 +1,24 @@
+from sklearn.linear_model import LinearRegression
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsRegressor
 
-df = pd.DataFrame(
-    [[4,7,10],
-    [5,8,11],
-     [6,9,12]] , columns = ["a","b","c"]
-)
+ls = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
+print(ls)
+# print(type(ls))
+x = ls[["GDP per capita (USD)"]].values
+y = ls[["Life satisfaction"]].values
+#print(y)
 
-print(df)
+# ls.plot(kind = 'scatter', grid =True, x= 'GDP per capita (USD)', y= 'Life satisfaction')
+# plt.axis([23_500,62_500,4,9])
+# plt.show()
+
+# model = LinearRegression()
+model = KNeighborsRegressor(n_neighbors=3)
+model.fit(x,y)
+x_new = [[37_655.2]]
+print(model.predict(x_new))
+# LinearRegression 6.30
+# KNeighbootRegressor 6.33
